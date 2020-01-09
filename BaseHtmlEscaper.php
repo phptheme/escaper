15cb068f46352eb09ce6f060a968d98c22d5a18a
phptheme <dev@getphptheme.com>
2020-01-09 16:21:09 +0300

    n/a

---
+++ BaseHtmlEscaper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author PhpTheme Dev Team <dev@getphptheme.com>
+ * @license MIT
+ * @link http://getphptheme.com
+ */
+namespace PhpTheme\HtmlEscaper;
+
+abstract class BaseHtmlEscaper
+{
+
+    public static function escape($string, $encoding = 'utf-8', $specialCharsFlags = ENT_QUOTES | ENT_SUBSTITUTE)
+    {
+        return htmlspecialchars($string, $specialCharsFlags, $encoding);
+    }
+
+}
